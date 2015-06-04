5598daf5f492891ff9f296f3253d310dde2dfaa0
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 19:21:40 +0700

    add private function

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,9 +8,33 @@ class HomeController extends BaseController {
             $homepage = $domain->homepage ? $domain->homepage : $domain->domain;
             return View::make('home')->withHomepage($homepage);
         }else{
-        	$user_now = Profile::where('user_agent',$_SERVER['HTTP_USER_AGENT'])->where('remote_addr',$_SERVER['REMOTE_ADDR'])->first();
-            return View::make('home')->with('user_now',$user_now);
+        	$profile = $this->_getProfile();
+            return View::make('home')->with('profile',$profile);
         }
+	}
+	
+	public function postIndex()
+	{
+        $attend = new Attend;
+        $attend->user_id = $this->_getUserId();
+        $attend->remote_addr = $_SERVER['REMOTE_ADDR'];
+        $attend->user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $attend->save();
+        return Output::push(array(
+							'path' => '/',
+							'messages' => array('success' => _('Thank You')),
+							));    
+	}
+	
+	private function _getUserId(){
+		$user_now = Profile::where('user_agent',$_SERVER['HTTP_USER_AGENT'])->where('remote_addr',$_SERVER['REMOTE_ADDR'])->first();
+		$user = User::where('profile_id',$user_now->id)->first();
+		return $user->id;
+	}
+	
+	private function _getProfile(){
+		$profile = Profile::where('user_agent',$_SERVER['HTTP_USER_AGENT'])->where('remote_addr',$_SERVER['REMOTE_ADDR'])->first();
+		return $profile;
 	}
 
 }
