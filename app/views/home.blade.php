401e5b4bfe90a8e29c224baf3cf675895b64c8fc
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 16:19:50 +0700

    edit text

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -11,21 +11,21 @@
 		@if(Cookie::get('domain_hash'))
 		{{ $homepage }}
 		@else
-		<div id="clock" class="light">
+		
+		
+		
+		<div class="well">
+			
+			<div id="clock" class="light">
 			<div class="display">
 				<div class="weekdays"></div>
 				<div class="ampm"></div>
 				<div class="alarm"></div>
 				<div class="digits"></div>
 			</div>
-		</div>
-		
-		<div class="well">
-			<p>Ngikut homepage</p>
+			</div>
+			<p>Saya Masuk</p>
 			
-			<p>Ini merupakan home page yang kekinian, simple tapi cetar membahana. Pastikan anda memakainya kalo tidak sangat sangat rugi</p>
-			
-			<p>Edit this page by editing <strong>app/views/home.blade.php</strong></p>
 		</div>
 		@endif
 	</div>	
