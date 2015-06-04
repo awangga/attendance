804577a3eb29352db64991088c85ab5d1b1a4765
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 14:27:25 +0700

    change db config for mysql

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'ams',
+			'username'  => 'ams',
+			'password'  => 'ams',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
