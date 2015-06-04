4fb662805db08ceb00aac9bd5565b74a9d005411
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 16:35:59 +0700

    add fillable column agent and addr

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -2,7 +2,7 @@
 
 class Profile extends Eloquent {
 
-	protected $fillable = array('first_name', 'last_name', 'email', 'website', 'address');
+	protected $fillable = array('first_name', 'last_name', 'email', 'phone_number', 'remote_addr', 'user_agent');
     protected $softDelete = true;
 
 	public function user() {
