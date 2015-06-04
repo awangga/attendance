1ce22e223b4d81a38f9cb81204718e4ab65389f5
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 14:11:54 +0700

    make migration of profiles table

---
+++ app/database/migrations/2015_06_04_070604_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$this->down();
+		Schema::create('profiles', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('first_name', 255);
+			$table->string('last_name', 255);
+			$table->string('website', 255);
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+			$table->timestamp('deleted_at')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('profiles');
+	}
+
+}
