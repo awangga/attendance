ee15008ae45c11d0418dabdf3969accd3a538fc4
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 16:53:29 +0700

    add email setting for localhost

--- app/config/mail.php
+++ app/config/mail.php
@@ -15,7 +15,7 @@ return array(
 	|
 	*/
 
-	'driver' => 'smtp',
+	'driver' => 'mail',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => '127.0.0.1',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return array(
 	|
 	*/
 
-	'port' => 587,
+	'port' => 25,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'ngabsen@ngikut.com', 'name' => 'Ngikut Ngabsen'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return array(
 	|
 	*/
 
-	'encryption' => 'tls',
+	'encryption' => '',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => '',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => '',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -122,3 +122,12 @@ return array(
 	'pretend' => false,
 
 );
+if(Schema::hasTable('settings')) {
+
+    $mail['from'] = array(
+    'address' => Config::get('settings.email_address_for_notification'), 
+    'name' => Config::get('settings.sender_name')
+    );
+}
+
+return $mail;
