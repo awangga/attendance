a93d9e7a38644190b500779cea039d2aeb8082a2
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-04 15:18:24 +0700

    test controller

--- app/controllers/TestController.php
+++ app/controllers/TestController.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeController extends BaseController {
+class TestController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
