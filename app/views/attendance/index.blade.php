793f0338946b24f644b991319e72e1b830fb9669
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-05 09:37:34 +0700

    add attendance

---
+++ app/views/attendance/index.blade.php
@@ -0,0 +1,31 @@
+@extends('template.skeleton')
+
+@section('title')
+{{ Auth::user()->username.' | '._('Online phones') }}
+@stop
+
+@section('content')
+
+	<div class="container">
+
+		<h1>{{ _('Attendances') }}</h1>
+        <p>Last update: {{ count($attends) > 0 ? $attends[0]->created_at : ''}}</p>
+
+		@include('template.messages')
+
+		<table id="enable_pagination" class="table table-striped table-hover dt-responsive" cellspacing="0" width="100%">
+                <thead><tr>
+                	<th>{{ _('Checkin') }}</th>
+                    <th>{{ _('Login From') }}</th>
+                </tr></thead><tbody>
+                @foreach ($attends as $attend)
+                <tr>
+                	<td>{{ $attend->created_at }}</td>
+                    <td>{{ $attend->remote_addr }}</td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+		
+	</div>
+@stop
