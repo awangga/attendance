f75c0ffdf0d722107422f8dbae961f78b12f40c3
Rolly Maulana Awangga <rolly@awang.ga>
2015-06-06 18:31:16 +0700

    fix bug if early come

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -14,33 +14,35 @@
 		
 		
 		
-		<div class="well">	
-					<div id="clock" class="light">
-					<div class="display">
-						<div class="weekdays"></div>
-						<div class="ampm"></div>
-						<div class="alarm"></div>
-						<div class="digits"></div>
-					</div>
-					</div>
-					<br>
-					<p class="col-xs-6 col-sm-4">
-					{{ Form::open(array('url' => '/', 'method' => 'post', 'class'=> 'form-inline')) }}
-					@if ($started == 0)
-					{{ Form::submit('Start as '.$profile->first_name, array('class' => 'btn btn-success btn-lg')) }}
-					@endif
-					@if ($started == 1)
-					{{ Form::submit('Stop as '.$profile->first_name, array('class' => 'btn btn-danger btn-lg')) }}
-					@endif
-					{{ Form::close() }}
-					<a href="{{ url('login') }}"><span class="glyphicon glyphicon-remove"></span>{{ _("it's not me") }}</a></p>
-		</div>
-		<div class="well">
-		<h3>Congratulations!!!</h3>
-		<p>
-			Early Come Today : {{$today_early->first_name.' '.$today_early->last_name}}</p>
-		</p>
-		</div>
+			<div class="well">	
+						<div id="clock" class="light">
+						<div class="display">
+							<div class="weekdays"></div>
+							<div class="ampm"></div>
+							<div class="alarm"></div>
+							<div class="digits"></div>
+						</div>
+						</div>
+						<br>
+						<p class="col-xs-6 col-sm-4">
+						{{ Form::open(array('url' => '/', 'method' => 'post', 'class'=> 'form-inline')) }}
+						@if ($started == 0)
+						{{ Form::submit('Start as '.$profile->first_name, array('class' => 'btn btn-success btn-lg')) }}
+						@endif
+						@if ($started == 1)
+						{{ Form::submit('Stop as '.$profile->first_name, array('class' => 'btn btn-danger btn-lg')) }}
+						@endif
+						{{ Form::close() }}
+						<a href="{{ url('login') }}"><span class="glyphicon glyphicon-remove"></span>{{ _("it's not me") }}</a></p>
+			</div>
+			@if(!is_null($today_early->first_name))
+			<div class="well">
+			<h3>Congratulations!!!</h3>
+			<p>
+				Early Come Today : {{$today_early->first_name.' '.$today_early->last_name}}</p>
+			</p>
+			</div>
+			@endif
 		@endif
 	</div>	
 @stop
